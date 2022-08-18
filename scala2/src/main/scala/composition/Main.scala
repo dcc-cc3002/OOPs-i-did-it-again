cb774bdbf24ae8e723807ec25ba8407209f95e61
R8V <ignacio.slater@ug.uchile.cl>
2022-08-18 17:17:46 -0400

    Composition Scala 2

---
+++ scala2/src/main/scala/composition/Main.scala
@@ -0,0 +1,27 @@
+package cl.dcc.uchile.oop
+package composition
+
+/*
+ * "oops-i-did-it-again" (c) by Ignacio Slater M.
+ * "oops-i-did-it-again" is licensed under a
+ * Creative Commons Attribution 4.0 International License.
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val player = new Player()
+    val mildTurkey = new Card("Mild Turkey", "The taste of victory will bowl you over.")
+    val dragoonsOfDraconia = new Card(
+      "Dragoons of Draconia", "Armed with muskets and iron spears, these mounted land " +
+        "troops of the Draconia Empire are feared by the Reptier Kingdom and other bordering " +
+        "nations."
+    )
+    player.deck += mildTurkey
+    player.deck += dragoonsOfDraconia
+    for (card <- player.deck) {
+      println(s"${card.name} - ${card.text}")
+    }
+  }
+}
---
+++ scala2/src/main/scala/composition/Player.scala
@@ -0,0 +1,15 @@
+/*
+ * "oops-i-did-it-again" (c) by Ignacio Slater M.
+ * "oops-i-did-it-again" is licensed under a
+ * Creative Commons Attribution 4.0 International License.
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+package cl.dcc.uchile.oop
+package composition
+
+import scala.collection.mutable.ArrayBuffer
+
+class Player {
+   var deck = new ArrayBuffer[Card]()
+}
